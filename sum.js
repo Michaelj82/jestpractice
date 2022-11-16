06f8bd43fca7eb7e8384b1e7a2ff5c940bbb6a04
Michael Johnson <mjj.yoyogi@gmail.com>
2022-11-15 21:54:15 -0500

    Made test module, made function module, all functions passed.

--- sum.js
+++ sum.js
@@ -1,10 +1,17 @@
-function capitalize(){
-    
-}
+const { assertDeclareInterface } = require("@babel/types");
+const { number } = require("yargs");
 
-function reverseString(){
+module.exports = {}
 
-}
+module.exports.capitalize = function capitalize(word){
+    let answer = word.toUpperCase()
+    return answer
+};
+
+module.exports.reverseString = function reverseString(statement){
+    let answer = statement.split('').reverse().join('')
+    return answer
+};
 
 const calculator = (num1, num2) => {
     let state = {
@@ -15,24 +22,75 @@ const calculator = (num1, num2) => {
     function add(){
         return num1 + num2
     }
+
+    function subtract(){
+        return num1-num2
+    }
+
+    function divide(){
+        return num1/num2
+    }
     
+    function multiply(){
+        return num1 * num2
+    }
+
 
     return Object.assign(
-        {state, add}
+        {state, add, subtract, divide, multiply}
     )
+}
+module.exports.calculator = calculator
 
 
+
+
+
+module.exports.caesarCipher = function caesarCipher(word){
+    let plain = [...Array(26)].map((_, i) => String.fromCharCode(i + 97)); 
+    let caesar = [...plain]
+    caesar = caesar.concat(caesar.splice(0,1))
+
+    let solution = word.split('')
+
+    for (let i =0; i < solution.length; i++){
+        let char = solution[i]
+        let index = plain.indexOf(char)
+
+        let correct = caesar[index]
+
+        solution[i] = correct
+
+    }
+
+    solution = solution.join('')
+
+    
+
+
+    return solution
+
+}
+
+module.exports.analyzeArray = function analyzeArray(numberList){
+
+    let sorted = numberList.sort(function(a, b){
+        return a-b;
+    })
+
+    const sum = numberList.reduce((partialSum, a) => partialSum + a, 0)
+
+
+    let solution = {
+        average: (sum/(numberList.length)),
+        min: sorted[0],
+        max: sorted[(sorted.length - 1)],
+        length: numberList.length
+    }
+
+    return solution
 
 
 }
 
-function caesarCipher(){
-
-}
-
-function analyzeArray(){
-
-}
-
-module.exports = sum;
 
--- sum.test.js
+++ sum.test.js
@@ -1,21 +1,40 @@
-const { default: test } = require('node:test');
-const sum = require('./sum');
+// const capitalize = require('./sum');
+let myModule = require('./sum');
+
 
 test('returns entire given string capitalized', () => {
-    expect(capitalize('joseph')).toBe('JOSEPH');
+    let solution = myModule.capitalize('joseph');
+    expect(solution).toBe('JOSEPH');
 });
 
 test('reverses string from backwards to forwards', () => {
-    expect(reverseString('joseph').toBe('hpesoj'));
+    let answer = myModule.reverseString('lamp')
+    expect(answer).toBe('pmal');
 
 });
 
 test('Completes all mathematical procedures correctly with 2 numbers', () => {
 
-    expect(calculator(1,2).add().toBe(3));
-    expect(calculator(3,2).subtract().toBe(1));
-    expect(calculator(4,2).divide().toBe(2));
-    expect(calculator(2,2).multiply().toBe(4));
+    expect(myModule.calculator(1,2).add()).toBe(3);
+    expect(myModule.calculator(3,2).subtract()).toBe(1);
+    expect(myModule.calculator(4,2).divide()).toBe(2);
+    expect(myModule.calculator(2,2).multiply()).toBe(4);
+
+
+})
+
+test('Does Caesar Cipher correctly, shifting all letters -1', () => {
+
+    expect(myModule.caesarCipher('zebra')).toBe('afcsb')
+
+})
+
+test('Returns function from list of numbers with correct average, min, max, and length', () => {
+    let object = myModule.analyzeArray([1,8,3,4,2,6]);
+    expect((object.average)).toBe(4)
+    expect((object.min)).toBe(1)
+    expect((object.max)).toBe(8)
+    expect((object.length)).toBe(6)
 
 
 })
